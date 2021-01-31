9cab4e736e6c0b61c6d5ab051de66c14317032da
pixelove <pixeloved@gmail.com>
2021-01-31 16:27:55 +0100

    Event and Property Binding added, so I can switch between Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe(`A test recipe`, `this is just a test`, `https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/01/lasagne-768x960.jpg`)
+    new Recipe(`A test recipe`, `this is just a test`, `https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/01/lasagne-768x960.jpg`),
+    new Recipe(`Another test recipe`, `this is just a test`, `https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/01/lasagne-768x960.jpg`)
   ];
 
   constructor() { }
@@ -15,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  };
 }
